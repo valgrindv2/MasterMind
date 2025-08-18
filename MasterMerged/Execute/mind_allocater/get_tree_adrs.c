cc5f5cd8306542f9b78108313e5aaa9282f8e029
Valgrind <oimzilene@gmail.com>
2025-08-18 14:06:32 +0100

    Mind alloc normed

--- MasterMerged/Execute/mind_allocater/get_tree_adrs.c
+++ MasterMerged/Execute/mind_allocater/get_tree_adrs.c
@@ -1,54 +1,49 @@
 #include "../execute.h"
 
-static void add_arg_list_to_gc(t_arg *arg)
+static void	add_arg_list_to_gc(t_arg *arg)
 {
-    while (arg)
-    {
-        if (arg->value)
-            allocate_gc(arg->value);
-        allocate_gc(arg);
-        arg = arg->next;
-    }
+	while (arg)
+	{
+		if (arg->value)
+			allocate_gc(arg->value);
+		allocate_gc(arg);
+		arg = arg->next;
+	}
 }
 
-static void add_red_list_to_gc(t_red *red)
+static void	add_red_list_to_gc(t_red *red)
 {
-    while (red)
-    {
-        if (red->value)
-            allocate_gc(red->value);
-        allocate_gc(red);
-        red = red->next;
-    }
+	while (red)
+	{
+		if (red->value)
+			allocate_gc(red->value);
+		allocate_gc(red);
+		red = red->next;
+	}
 }
 
-static void add_double_str_to_gc(char **arr)
+static void	add_double_str_to_gc(char **arr)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (!arr)
-        return;
-    while (arr[i])
-        allocate_gc(arr[i++]);
-    allocate_gc(arr);
+	i = 0;
+	if (!arr)
+		return ;
+	while (arr[i])
+		allocate_gc(arr[i++]);
+	allocate_gc(arr);
 }
 
-// this function needs to be adapted, if a malloc fails in the middle the rest
-// of the tree isnt allocated so it should be freed alone 
-// readapt allocate_gc to not exit version and return fail free the collected so far
-// and free tree and exit.
-void tree_gc_collector(t_tree *node)
+void	tree_gc_collector(t_tree *node)
 {
-    if (!node)
-        return;
-    if (node->value)
-        allocate_gc(node->value);
-    add_double_str_to_gc(node->argv);
-    add_arg_list_to_gc(node->arg);
-    add_red_list_to_gc(node->red);
-    allocate_gc(node);
-
-    tree_gc_collector(node->left);
-    tree_gc_collector(node->right);
+	if (!node)
+		return ;
+	if (node->value)
+		allocate_gc(node->value);
+	add_double_str_to_gc(node->argv);
+	add_arg_list_to_gc(node->arg);
+	add_red_list_to_gc(node->red);
+	allocate_gc(node);
+	tree_gc_collector(node->left);
+	tree_gc_collector(node->right);
 }
