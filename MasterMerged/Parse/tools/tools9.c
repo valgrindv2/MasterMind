dc1a5c8040a173bca6e943dfafde9a76aa7fad1f
Valgrind <oimzilene@gmail.com>
2025-08-22 19:35:03 +0100

    Fixed the invalid close in heredoc data->here_fd

--- MasterMerged/Parse/tools/tools9.c
+++ MasterMerged/Parse/tools/tools9.c
@@ -6,13 +6,22 @@
 /*   By: ayel-bou <ayel-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 20:48:04 by ayel-bou          #+#    #+#             */
-/*   Updated: 2025/08/16 07:17:37 by ayel-bou         ###   ########.fr       */
+/*   Updated: 2025/08/22 19:33:37 by ayel-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 extern int	g_flag;
+
+static void closing_here_fd(t_data *data)
+{
+	if (data->here_fd != -1)
+	{
+		close(data->here_fd);
+		data->here_fd = -1;
+	}
+}
 
 int	list_size(t_token *list)
 {
@@ -46,8 +55,7 @@ static void	single_interruption(char *in, t_data *data)
 {
 	puterror("Master@Mind: Single Heredoc Blocked\n");
 	data->exit_status = 0;
-	close(data->here_fd);
-	data->here_fd = -1;
+	closing_here_fd(data);
 	free(in);
 }
 
@@ -72,8 +80,7 @@ int	here_doc_ops(t_token *id_class, t_data *data, char *del)
 		in = readline("Here_doc> ");
 	}
 	close(sv);
-	close(data->here_fd);
-	data->here_fd = -1;
+	closing_here_fd(data);
 	signal(SIGINT, sig_handler);
 	if (!store_fd(id_class->next, data))
 		return (free(in), 0);
